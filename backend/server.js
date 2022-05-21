add1d9adeb3beb21904b0c08904bf5eaaae9a146
An Cao <pullnolovesmile@gmail.com>
2022-05-21 22:29:42 +0700

    init source

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const express = require("express")
 const cors = require("cors")
 const multer = require("multer")
 const app = express()
-const PORT = 3002
+const DatauriParser = require('datauri/parser');
+const cloudinary = require("cloudinary").v2;
+const PORT = 3004
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 /**
  * Use upload picture lib multer
@@ -37,34 +39,73 @@ const upload = multer({
  * multer sinlge upload
  */
 
-const singleUpload = upload.single('formFile')
-const singleUploadControl = (req, res, next) => {
-    singleUpload(req, res, (error) => {
-        if(error) {
-            return res.status(442).send({
-                message: "Image upload failed"
-            });
-        }
-        next();
-    });
+ const singleUpload = upload.single("file");
+ const singleUploadCtrl = (req, res, next) => {
+   singleUpload(req, res, (error) => {
+     if (error) {
+       return res.status(422).send({
+         message: "Image upload failed",
+       });
+     }
+     next();
+   });
+ };
+
+// use dataurl upload form buffer
+const parser = new DatauriParser();
+const path = require("path");
+const formatBuffer = (file) => {
+    return parser.format(
+        path.extname(file.originalname).toString().toLowerCase(),
+        file.buffer
+    )
 }
 
 
-
-
-
 app.get("/api", (res, req) => {
-    console.log("Hello nodejs")
+    console.log("Hello nodejs 123")
 })
 
 // upload api
-app.post("api/upload", singleUploadControl, (req, res) => {
-    console.log(req.file)
-    return res.send({
-        message: "Image send"
-    })
-})
-
-app.listen(PORT, () => {
-    console.log(`Server running port: ${PORT}`)
-})
+app.post("/api/upload", singleUploadCtrl, async (req, res) => {
+    //axios se co request la req.body
+    const uploadFile = req.body.file || req.file;
+    console.log(uploadFile);
+    try {
+        if (!uploadFile) {
+            return res.status(422).send({
+            message: "There are error in uploading",
+            });
+        }
+        let file64 = formatBuffer(uploadFile);
+        console.log(file64)
+        return res.json({
+            // cloudinaryId: uploadResult.public_id,
+            // url: uploadResult.secure_url,
+            message: "upload OK",
+        });
+  
+      // Chuyen file dang buffer sang base64
+    //   let uploadResult;
+    //   if (!uploadFile.buffer) {
+    //     uploadResult = await cloudinaryUpload(uploadFile);
+    //   } else {
+    //     let file64 = formatBuffer(uploadFile);
+    //     uploadResult = await cloudinaryUpload(file64.content);
+    //   }
+  
+    //   return res.json({
+    //     cloudinaryId: uploadResult.public_id,
+    //     url: uploadResult.secure_url,
+    //     message: "upload OK",
+    //   });
+    } catch (err) {
+      return res.status(422).send({
+        message: err.message,
+      });
+    }
+  });
+  
+  app.listen(PORT, () => {
+    console.log("Port is running on", PORT);
+  });
