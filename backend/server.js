801ed7f2d4a66667f79a8fd9d87155963ce1ec37
An Cao <pullnolovesmile@gmail.com>
2022-05-23 22:50:52 +0700

    init source

--- backend/server.js
+++ backend/server.js
@@ -1,111 +1,106 @@
-const express = require("express")
-const cors = require("cors")
-const multer = require("multer")
-const app = express()
-const DatauriParser = require('datauri/parser');
-const cloudinary = require("cloudinary").v2;
-const PORT = 3004
+//https://stackoverflow.com/questions/61716903/getting-req-body-empty-using-form-data-in-postman
+//https://stackoverflow.com/questions/25143525/can-we-configure-multer-to-not-store-files-locally-and-only-for-using-req-files
+/*
+Tại đây có 2 cách dùng multer: dùng inMemory để lưu file trên Ram, sau đó phải dùng stream để upload (?)
+https://codeburst.io/image-upload-with-cloudinary-part-2-next-react-node-js-198108f672e5
+Cách 2 là dùng fs sau khi upload thì xoá file
+*/
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-/**
- * Use upload picture lib multer
- * 
- */
+const { upload } = require("./utils/multerServices");
+const {
+  cloudinaryUpload,
+  getImages,
+  searchImages,
+} = require("./utils/cloudinaryServices");
+const { formatBuffer } = require("./utils/datauriServices");
 
-const ALLOWED_FORMATS = [
-    "image/jpeg",
-    "image/jpg",
-    "image/png"
-]
+//Check base64 image
+// const base64regex =
+//   /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+const base64ImageCheck = (file) => file.includes("base64");
 
-// use memoryStorage for multi upload save ram
-const storage = multer.memoryStorage();
-
-const upload = multer({
-    storage,
-    fileFilter: function(req, file, cb) {
-        if(ALLOWED_FORMATS.includes(file.mimetype)) {
-            cb(null, true)
-        } else {
-            cb(new Error("Not support file type"), false)
-        }
-    }
-})
-
-/**
- * multer sinlge upload
- */
-
- const singleUpload = upload.single("file");
- const singleUploadCtrl = (req, res, next) => {
-   singleUpload(req, res, (error) => {
-     if (error) {
-       return res.status(422).send({
-         message: "Image upload failed",
-       });
-     }
-     next();
-   });
- };
-
-// use dataurl upload form buffer
-const parser = new DatauriParser();
-const path = require("path");
-const formatBuffer = (file) => {
-    return parser.format(
-        path.extname(file.originalname).toString().toLowerCase(),
-        file.buffer
-    )
-}
-
-
-app.get("/api", (res, req) => {
-    console.log("Hello nodejs 123")
-})
-
-// upload api
-app.post("/api/upload", singleUploadCtrl, async (req, res) => {
-    //axios se co request la req.body
-    const uploadFile = req.body.file || req.file;
-    console.log(uploadFile);
-    try {
-        if (!uploadFile) {
-            return res.status(422).send({
-            message: "There are error in uploading",
-            });
-        }
-        let file64 = formatBuffer(uploadFile);
-        console.log(file64)
-        return res.json({
-            // cloudinaryId: uploadResult.public_id,
-            // url: uploadResult.secure_url,
-            message: "upload OK",
-        });
-  
-      // Chuyen file dang buffer sang base64
-    //   let uploadResult;
-    //   if (!uploadFile.buffer) {
-    //     uploadResult = await cloudinaryUpload(uploadFile);
-    //   } else {
-    //     let file64 = formatBuffer(uploadFile);
-    //     uploadResult = await cloudinaryUpload(file64.content);
-    //   }
-  
-    //   return res.json({
-    //     cloudinaryId: uploadResult.public_id,
-    //     url: uploadResult.secure_url,
-    //     message: "upload OK",
-    //   });
-    } catch (err) {
+const singleUpload = upload.single("file");
+const singleUploadCtrl = (req, res, next) => {
+  singleUpload(req, res, (error) => {
+    if (error) {
       return res.status(422).send({
-        message: err.message,
+        message: "Image upload failed",
       });
     }
+    next();
   });
-  
-  app.listen(PORT, () => {
-    console.log("Port is running on", PORT);
+};
+
+// Get images
+app.get("/api/photos/all", async (req, res) => {
+  const results = await getImages();
+  return res.json({
+    results: results,
   });
+});
+
+app.get("/api/photos", async (req, res) => {
+  //axios truyen params thong qua res.query
+  const response = await searchImages(req.query.next_cursor || "");
+  const results = {
+    images: [],
+    next_cursor: null,
+  };
+  response.resources.forEach((item) => {
+    results.images.push({
+      public_id: item.public_id,
+      created_at: item.created_at,
+      secure_url: item.secure_url,
+    });
+  });
+  if (response.next_cursor) {
+    results.next_cursor = response.next_cursor;
+  }
+  return res.json({
+    results,
+  });
+});
+
+//Upload image
+
+//Upload API
+
+app.post("/api/upload", singleUploadCtrl, async (req, res) => {
+  const uploadFile = req.body.file || req.file;
+  try {
+    if (!uploadFile) {
+      return res.status(422).send({
+        message: "There is error when uploading",
+      });
+    }
+    let uploadResult;
+    if (!uploadFile.buffer) {
+      uploadResult = await cloudinaryUpload(uploadFile);
+    } else {
+      const file64 = formatBuffer(req.file);
+      uploadResult = await cloudinaryUpload(file64.content);
+    }
+    //Convert stream to base64 format
+    console.log(uploadResult)
+    return res.status(200).json({
+      message: "Upload OK!",
+    });
+  } catch (error) {
+    return res.status(422).send({
+      message: error.message,
+    });
+  }
+});
+
+const port = process.env.PORT || 3004;
+app.listen(port, () => {
+  console.log("Port is running on", process.env.PORT);
+});
